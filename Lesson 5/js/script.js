88552ce7350a2e4a410c587de730f1466e8b76af
Valeria Ivinskaya <ivinskaya89@gmail.com>
2019-12-09 21:29:50 +0200

    Lesson 5, completed

--- Lesson 5/js/script.js
+++ Lesson 5/js/script.js
@@ -1,23 +1,26 @@
-let li = document.querySelectorAll('.menu-item'),
-  menu = document.querySelector('.menu'),
-  lastLi = document.createElement('li'),
-  divTitle = document.getElementById('title'),
-  column = document.getElementsByClassName('column'),
-  divAdv = document.querySelector('.adv');
+let li = document.querySelectorAll(".menu-item"),
+  menu = document.querySelector(".menu"),
+  lastLi = document.createElement("li"),
+  divTitle = document.getElementById("title"),
+  column = document.getElementsByClassName("column"),
+  divAdv = document.querySelector(".adv"),
+  divPrompt = document.getElementById("prompt");
 
 //1)
 menu.insertBefore(li[2], li[1]);
 
-lastLi.classList.add('menu-item');
-lastLi.textContent = 'Пятый пункт';
+lastLi.classList.add("menu-item");
+lastLi.textContent = "Пятый пункт";
 
 menu.appendChild(lastLi);
 //2)
-document.body.style.background = 'url(img/apple_true.jpg) center no-repeat';
+document.body.style.background = "url(img/apple_true.jpg) center no-repeat";
 
 //3)
-divTitle.textContent = 'Мы продаем только подлинную технику Apple';
+divTitle.textContent = "Мы продаем только подлинную технику Apple";
 
 //4)
 column[1].removeChild(divAdv);
-console.log(column[0]);
+//5)
+let answer = prompt("Как вы относитесь к технике apple?", "");
+divPrompt.textContent = answer;
